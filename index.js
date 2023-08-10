942d77b325b86b9be292d77055adf6258a60c0a4
Jaiswal <ajaiswal@comscore.com>
2023-08-11 03:46:53 +0530

    pilot

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MainComponent from "./src/components/MainComponent";
+
+
+// const div = document.createElement("div");
+// div.innerHTML = "HELLO World From JS";
+
+const root = document.querySelector(".root");
+// root.appendChild(div);
+
+
+// using CDN - no need to install react/react-dom
+// const divReact = React.createElement("div", { className: "react-div" }, [
+//   React.createElement(
+//     "div",
+//     { className: "child1" },
+//     "Hello World from React 1"
+//   ),
+//   React.createElement(
+//     "div",
+//     { className: "child2" },
+//     "Hello World from React 2"
+//   ),
+// ]);
+
+const rootReact = ReactDOM.createRoot(root);
+rootReact.render(<MainComponent/>);
---
+++ src/components/MainComponent.js
@@ -0,0 +1,5 @@
+const MainComponent = () => {
+    return (<div>Hello World from React Hooks 8</div>)
+}
+
+export default MainComponent;
